d1ab57afec0c63518c906428ee770114df576fff
pascal <pascal.fuehrlich@student.hpi.de>
2017-12-07 17:35:56 +0100

    changed tests, removed checksum, arrow functions, var->const

--- index.js
+++ index.js
@@ -1,83 +1,45 @@
-var crypto = require('crypto');
-var path = require('path');
-var through = require('through2');
-var fs = require('fs');
+const path = require('path')
+const through = require('through2')
+const fs = require('fs')
 
-var GLOBAL_CACHE = {};
+const defaultCache = {}
 
 // look for changes by mtime
-function processFileByModifiedTime(stream, firstPass, basePath, file, cache) {
-  var newTime = file.stat && file.stat.mtime;
-  var filePath = basePath ? path.relative(basePath, file.path) : file.path;
-  var oldTime = cache[filePath];
+const processFile = (stream, basePath, file, cache) => {
+	const actualTime = (file.stat && file.stat.mtime).getTime()
+	const filePath = basePath ? path.relative(basePath, file.path) : file.path
+	const cacheTime = cache[filePath]
 
-  cache[filePath] = newTime.getTime();
+	cache[filePath] = actualTime
 
-  if (oldTime) {
-    if (oldTime !== newTime.getTime()) {
-        stream.push(file);
-    }
-  } else if (firstPass || newTime.getTime() > cache['.gulp-changed-smart']) {
-      stream.push(file);
-  }
+	if (cacheTime) {
+		if (cacheTime !== actualTime) {
+			stream.push(file)
+		}
+	} else if (actualTime > cache['.gulp-changed-smart']) {
+		stream.push(file)
+	}
 
-  fs.writeFileSync('.gulp-changed-smart', Date.now().toString());
+	fs.writeFileSync('.gulp-changed-smart', Date.now().toString())
 }
 
-// look for changes by sha1 hash
-function processFileBySha1Hash(stream, firstPass, basePath, file, cache) {
-  // null cannot be hashed
-  if (file.contents === null) {
-    // if element is really a file, something weird happened, but it's safer
-    // to assume it was changed (because we cannot said that it wasn't)
-    // if it's not a file, we don't care, do we? does anybody transform directories?
-    if (file.stat.isFile()) {
-      stream.push(file);
-    }
-  } else {
-    var newHash = crypto.createHash('sha1').update(file.contents).digest('hex');
-    var filePath = basePath ? path.relative(basePath, file.path) : file.path;
-    var currentHash = cache[filePath];
+module.exports = options => {
+	options = options || {}
 
-    cache[filePath] = newHash;
+	const basePath = options.basePath || undefined
+	const cache = options.cache || defaultCache
 
-    if ((!currentHash && firstPass) || (currentHash && currentHash !== newHash)) {
-      stream.push(file);
-    }
-  }
-}
+	if (!cache['.gulp-changed-smart']) {
+		if (fs.existsSync('.gulp-changed-smart')) {
+			const fileContent = fs.readFileSync('.gulp-changed-smart', { encoding: 'utf8' })
+			cache['.gulp-changed-smart'] = new Date(parseInt(fileContent))
+		} else {
+			cache['.gulp-changed-smart'] = new Date(0)
+		}
+	}
 
-module.exports = function (options) {
-  options = options || {};
-
-  var processFile;
-
-  switch (options.howToDetermineDifference) {
-    case 'hash':
-      processFile = processFileBySha1Hash;
-      break;
-    case 'modification-time':
-      processFile = processFileByModifiedTime;
-      break;
-    default:
-      processFile = processFileBySha1Hash;
-  }
-
-  var basePath = options.basePath || undefined;
-  var cache = options.cache || GLOBAL_CACHE;
-  var firstPass = options.firstPass === true;
-
-  if (!cache['.gulp-changed-smart'] && options.howToDetermineDifference === 'modification-time'){
-      if (fs.existsSync('.gulp-changed-smart')) {
-          var fileContent = fs.readFileSync('.gulp-changed-smart', {encoding: 'utf8'});
-          cache['.gulp-changed-smart'] = new Date(parseInt(fileContent));
-      } else {
-          cache['.gulp-changed-smart'] = new Date(0);
-      }
-  }
-
-  return through.obj(function (file, encoding, callback) {
-    processFile(this, firstPass, basePath, file, cache);
-    callback();
-  });
+	return through.obj(function(file, encoding, callback) {
+		processFile(this, basePath, file, cache)
+		callback()
+	})
 }
--- test.js
+++ test.js
@@ -1,224 +1,99 @@
-var path = require('path');
-var fs = require('fs');
-var assert = require('assert');
-var concatStream = require('concat-stream');
-var es = require('event-stream');
-var gulp = require('gulp');
-var changedSmart = require('./');
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+const concatStream = require('concat-stream')
+const es = require('event-stream')
+const gulp = require('gulp')
+const changedSmart = require('./')
 
-describe('gulp-changed-smart', function () {
+describe('gulp-changed-smart', () => {
 
-  describe('when comparing by sha1 hash', function () {
-    it('passes all files on start by default', function (done) {
-      gulp.src('fixture/*')
-        .pipe(changedSmart({ firstPass: true, cache: {} }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(2, buf.length);
-          assert.equal('a', path.basename(buf[0].path));
-          assert.equal('b', path.basename(buf[1].path));
-          done();
-        }));
-    });
+	it('should only pass through files when their modification time changed', done => {
+		const times = { '.gulp-changed-smart': new Date(0) }
 
-    it('does not pass all files on start with `firstPass: false`', function (done) {
-      gulp.src('fixture/*')
-        .pipe(changedSmart({ firstPass: false, cache: {} }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(0, buf.length);
-          done();
-        }));
-    });
+		const fileA = path.join(__dirname, 'fixture/a')
+		const fileB = path.join(__dirname, 'fixture/b')
 
-    it('should only pass through files when they change', function (done) {
-      var shas = {};
+		const timeNow = Date.now() / 1000 // https://nodejs.org/docs/latest/api/fs.html#fs_fs_utimes_path_atime_mtime_callback
 
-      shas[path.join(__dirname, 'fixture/a')] = 'not matching sha';
-      shas[path.join(__dirname, 'fixture/b')] = 'e9d71f5ee7c92d6dc9e92ffdad17b8bd49418f98';
+		const timeThen = new Date()
+		timeThen.setFullYear(timeThen.getFullYear() - 1)
 
-      gulp.src('fixture/*')
-        .pipe(changedSmart({ firstPass: false, cache: shas }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(1, buf.length);
-          assert.equal('a', path.basename(buf[0].path));
-          done();
-        }));
-    });
+		const fileBTime = fs.statSync(fileB).mtime.getTime()
+		fs.utimesSync(fileA, timeNow, timeNow) //does this work?
 
-    it('should update cache before pushing file to stream', function (done) {
-      var shas = {};
+		times[fileA] = timeThen
+		times[fileB] = fileBTime
 
-      gulp.src('fixture/*')
-        .pipe(changedSmart({ firstPass: true, cache: shas }))
-        .pipe(es.map(function (file, callback) {
-          // imitate gulp.dest without actualy writing files
-          // @see https://github.com/gulpjs/vinyl-fs/blob/master/lib/prepareWrite.js#L24
-          var targetBase = path.resolve(file.cwd, './build');
-          var targetPath = path.resolve(targetBase, file.relative);
-          file.base = targetBase;
-          file.path = targetPath;
-          callback(null, file);
-        }))
-        .pipe(concatStream(function (files) {
-          assert.equal(2, files.length, 'should be 2 files');
+		gulp.src('fixture/*')
+			.pipe(changedSmart({ cache: times }))
+			.pipe(concatStream(buf => {
+				assert.equal(1, buf.length)
+				assert.equal('a', path.basename(buf[0].path))
+				done()
+			}))
+	})
 
-          files.map(function (file) {
-            assert.equal(undefined, shas[file.path], 'path of changed file should not be in cache');
-          });
+	it('should update cache before pushing file to stream', done => {
+		const times = { '.gulp-changed-smart': new Date(0) }
 
-          done();
-        }));
-    });
+		gulp.src('fixture/*')
+			.pipe(changedSmart({ cache: times }))
+			.pipe(es.map((file, callback) => {
+				// imitate gulp.dest without actualy writing files
+				// @see https://github.com/gulpjs/vinyl-fs/blob/master/lib/prepareWrite.js#L24
+				const targetBase = path.resolve(file.cwd, './build')
+				const targetPath = path.resolve(targetBase, file.relative)
+				file.base = targetBase
+				file.path = targetPath
+				callback(null, file)
+			}))
+			.pipe(concatStream(files => {
+				assert.equal(2, files.length, 'should be 2 files')
 
-    it('should use paths relative to `basePath`', function (done) {
-      var shas = {};
-      var basePath = __dirname;
+				files.forEach(file => {
+					assert.equal(undefined, times[file.path], 'path of changed file should not be in cache')
+				})
 
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: true,
-          basePath: basePath,
-          cache: shas
-        }))
-        .pipe(concatStream(function (files) {
+				done()
+			}))
+	})
 
-          files.map(function (file) {
-            assert.equal(true, shas.hasOwnProperty(path.relative(basePath, file.path)), 'file path should be relative');
-          });
+	it('should use paths relative to `basePath`', done => {
+		const times = { '.gulp-changed-smart': new Date(0) }
+		const basePath = __dirname
 
-          done();
-        }));
-    });
-  });
+		gulp.src('fixture/*')
+			.pipe(changedSmart({
+				basePath: basePath,
+				cache: times
+			}))
+			.pipe(concatStream(files => {
 
-  describe('when comparing by file modification time: ', function () {
-    it('passes all files on start by default', function (done) {
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: true,
-          cache: {},
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(2, buf.length);
-          assert.equal('a', path.basename(buf[0].path));
-          assert.equal('b', path.basename(buf[1].path));
-          done();
-        }));
-    });
+				files.map(file => {
+					assert.equal(true, times.hasOwnProperty(path.relative(basePath, file.path)), 'file path should be relative')
+				})
 
-    it('does not pass all files on start with `firstPass: false`', function (done) {
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: false,
-          cache: {},
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(0, buf.length);
-          done();
-        }));
-    });
+				done()
+			}))
+	})
 
-    it('should only pass through files when their modification time changed', function (done) {
-      var times = {};
+	it('should only push files on startup that were changed since last cache update', done => {
+        const gulpChangedSmartFileContent = Date.now().toString()
+		const times = { '.gulp-changed-smart': new Date(parseInt(gulpChangedSmartFileContent)) }
+        const lastCacheUpdate = times['.gulp-changed-smart'].getTime() / 1000
 
-      var fileA = path.join(__dirname, 'fixture/a');
-      var fileB = path.join(__dirname, 'fixture/b');
+        const fileA = path.join(__dirname, 'fixture/a')
+        fs.utimesSync(fileA, 0, lastCacheUpdate + 1) //changed since last cache update
+        const fileB = path.join(__dirname, 'fixture/b')
+        fs.utimesSync(fileB, 0, lastCacheUpdate - 1) //not changed since last cache update
 
-      var timeNow = Date.now() / 1000;  // https://nodejs.org/docs/latest/api/fs.html#fs_fs_utimes_path_atime_mtime_callback
-
-      var timeThen = new Date();
-      timeThen.setFullYear(timeThen.getFullYear() - 1);
-
-      var fileBTime = fs.statSync(fileB).mtime;
-      fs.utimesSync(fileA, timeNow, timeNow);
-
-      times[fileA] = timeThen;
-      times[fileB] = fileBTime.getTime();
-
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: false,
-          cache: times,
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(concatStream(function (buf) {
-          assert.equal(1, buf.length);
-          assert.equal('a', path.basename(buf[0].path));
-          done();
-        }));
-    });
-
-    it('should update cache before pushing file to stream', function (done) {
-      var times = {};
-
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: true,
-          cache: times,
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(es.map(function (file, callback) {
-          // imitate gulp.dest without actualy writing files
-          // @see https://github.com/gulpjs/vinyl-fs/blob/master/lib/prepareWrite.js#L24
-          var targetBase = path.resolve(file.cwd, './build');
-          var targetPath = path.resolve(targetBase, file.relative);
-          file.base = targetBase;
-          file.path = targetPath;
-          callback(null, file);
-        }))
-        .pipe(concatStream(function (files) {
-          assert.equal(2, files.length, 'should be 2 files');
-
-          files.map(function (file) {
-            assert.equal(undefined, times[file.path], 'path of changed file should not be in cache');
-          });
-
-          done();
-        }));
-    });
-
-    it('should use paths relative to `basePath`', function (done) {
-      var times = {};
-      var basePath = __dirname;
-
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: true,
-          basePath: basePath,
-          cache: times,
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(concatStream(function (files) {
-
-          files.map(function (file) {
-            assert.equal(true, times.hasOwnProperty(path.relative(basePath, file.path)), 'file path should be relative');
-          });
-
-          done();
-        }));
-    });
-
-    it('should only push files on startup that were changed after last cache update', function (done) {
-      var times = {};
-      var basePath = __dirname;
-      var gulpChangedSmartFileContent = (Date.now() - 1).toString();
-      times['.gulp-changed-smart'] = new Date(parseInt(gulpChangedSmartFileContent));
-      var aFileContent = fs.readFileSync('fixture/a', {encoding: 'utf8'});
-      fs.writeFileSync('fixture/a', aFileContent);
-
-      gulp.src('fixture/*')
-        .pipe(changedSmart({
-          firstPass: false,
-          basePath: basePath,
-          cache: times,
-          howToDetermineDifference: 'modification-time'
-        }))
-        .pipe(concatStream(function (files) {
-          assert.equal(1, files.length, 'exactly one file was changed since last cache update');
-          assert.equal('a', path.basename(files[0].path));
-          done();
-        }));
-    });
-  });
-});
+		gulp.src('fixture/*')
+			.pipe(changedSmart({ cache: times }))
+			.pipe(concatStream(files => {
+				assert.equal(1, files.length, 'exactly one file was changed since last cache update')
+				assert.equal('a', path.basename(files[0].path))
+				done()
+			}))
+	})
+})
